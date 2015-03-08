1a2058c394d471eff7cfdc67737f8c08df6b8ba5
NaglijLamer <NL40.000@mail.ru>
2015-03-08 07:24:33 +0300

    Fix some error messages.

--- Interpreter/instant_exit.c
+++ Interpreter/instant_exit.c
@@ -10,11 +10,11 @@ void program_crash(int error_code)
 	switch(error_code)
 	{
 		case inval_instr: fputs("Error! Invalid instruction.", stderr); break;
-		case stck_overflow: fputs("Error! Stack overflow", stderr); break;
+		case stck_overflow: fputs("Error! Stack overflow!", stderr); break;
 		case stck_empt: fputs("Error! Stack is empty or not enough arguments!", stderr); break;
 		case wrng_file: fputs("Error! There is not an executable file!", stderr); break;
 		case ctx_empt: fputs("Error! The appeal to a context which doesn't exist or context is broken!", stderr); break;
-		case ctx_overflow: fputs("Error! Too many frames for one function!", stderr); break;
+		case ctx_overflow: fputs("Error! Too many frames - context stack overflow!", stderr); break;
 		case no_entr_funct: fputs("Error! Can not find an entry point!", stderr); break;
 		case wrng_fname: fputs("Error! Can't open or find this file!", stderr); break;
 		case usage: fputs("Usage: interpreter filename", stderr); break;
